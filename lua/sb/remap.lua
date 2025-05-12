f754ffcde4871ab2a5ee3f54b5e79b3d6f73c177
pacificseabass <releasetheseabass@gmail.com>
2025-05-11 21:41:33 -0700

    Add remappings for window switching

--- lua/sb/remap.lua
+++ lua/sb/remap.lua
@@ -1,3 +1,11 @@
 vim.g.mapleader = " "
 
+-- window switching
+vim.keymap.set('n', '<leader>w|', ':vsplit<CR>')
+vim.keymap.set('n', '<leader>w-', ':hsplit<CR>')
+vim.keymap.set('n', '<leader>wh', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>wj', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>wk', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>wl', '<C-w>l', { noremap = true, silent = true })
+
 
