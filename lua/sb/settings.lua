cca2718079ee6f1f44c0eab763e8727089c1cce4
pacificseabass <releasetheseabass@gmail.com>
2025-06-10 11:59:14 -0700

    = format settings.lua

--- lua/sb/settings.lua
+++ lua/sb/settings.lua
@@ -1,6 +1,6 @@
 vim.opt.tabstop = 2
-vim.softtabstap = 0 	-- (0 for 'tabstop, -1 for 'shiftwidth')
-vim.opt.shiftwidth = 2 -- number of spaces to use for each step of (auto)indent
+vim.softtabstap = 0      -- (0 for 'tabstop, -1 for 'shiftwidth')
+vim.opt.shiftwidth = 2   -- number of spaces to use for each step of (auto)indent
 vim.opt.expandtab = true -- expand TABs to spaces
 
 vim.o.exrc = true
