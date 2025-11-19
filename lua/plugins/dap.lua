69555d0990a8aa77ddc755b372c09d6094de3fe7
pacificseabass <releasetheseabass@gmail.com>
2025-11-18 20:47:22 -0800

    + Dap configuration

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "leoluz/nvim-dap-go",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      local dap = require "dap"
+      local ui = require "dapui"
+
+      require("dapui").setup()
+      require("dap-go").setup()
+
+      vim.keymap.set("n", "<space>db", dap.toggle_breakpoint)
+      -- vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+      -- Eval var under cursor
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set("n", "dc", dap.continue)
+      vim.keymap.set("n", "ds", dap.step_into)
+      vim.keymap.set("n", "dn", dap.step_over)
+      vim.keymap.set("n", "do", dap.step_out)
+      vim.keymap.set("n", "dm", dap.step_back)
+      vim.keymap.set("n", "dr", dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
+}
